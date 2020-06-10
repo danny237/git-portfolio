d033adb86375736a29358bcb32499bf3bd7214c6
Daniel Thapa Magar <thapadaniel23@gmail.com>
2020-06-10 23:11:38 +0545

    removing video auto-play and adding default_master in repos list

--- main.js
+++ main.js
@@ -19,10 +19,11 @@ fetch('https://api.github.com/users/danny237/repos')
     .then(response => response.json())
     .then(function(data) {
         for (let i = 0; i < 5; i++) {
-            projectname = data[i].name
-            link = data[i].html_url
+            let projectname = data[i].name
+            let link = data[i].html_url
+            let default_branch = data[i].default_branch
             document.getElementById('list').innerHTML += `<div class="box">${i+1}.Project- ${projectname}<br>
-            <a href="${link}" target=_blank>Click here</a>
+            <small style = "font-size: 10px;margin-right: 10px;">Default Branch:${default_branch} </small> <a href="${link}" target=_blank>Click here</a>
             </div>`
         }
         document.getElementById('loading').hidden = true;
